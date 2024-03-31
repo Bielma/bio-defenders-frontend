487a9c15ec51c67022bc61fe38d4c85f7b5e15d0
Mauricio Silva <mauricioramonsilvasandoval@gmail.com>
2024-03-30 21:51:34 -0600

    💥 Add theme.jsx, StyleText.jsx and Fonts
    Agregue las fuentes de nunito sans, las de work sans, y creación de un archivo theme, con (colors, fontSizes, fonts, fontWeights) así con el archivo react-native.config

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import Constants from 'expo-constants';
+
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+import Fuentes from './src/views/Fuentes'
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync()
+      .catch((err) => console.warn(err));
+
+    loadFonts()
+      .then(() => setFontsLoaded(true))
+      .catch((err) => console.warn(err));
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>BIODEFENDERS</Text>
-      <StatusBar style="auto" />
+      <Fuentes />
     </View>
   );
 }
@@ -18,3 +38,25 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+async function loadFonts() {
+  await Font.loadAsync({
+    //nunito sans fonts
+    'NunitoSans-Medium': require('./assets/fonts/NunitoSans_Medium.ttf'),
+    'NunitoSans-Regular': require('./assets/fonts/NunitoSans_Regular.ttf'),
+    'NunitoSans-Bold': require('./assets/fonts/NunitoSans_Bold.ttf'),
+    'NunitoSans-SemiBold': require('./assets/fonts/NunitoSans_SemiBold.ttf'),
+    'NunitoSans-ExtraBold': require('./assets/fonts/NunitoSans_ExtraBold.ttf'),
+    'NunitoSans-Black': require('./assets/fonts/NunitoSans_Black.ttf'),
+    //work sans fonts
+    'WorkSans-Medium': require('./assets/fonts/WorkSans-Medium.ttf'),
+    'WorkSans-Regular': require('./assets/fonts/WorkSans-Regular.ttf'),
+    'WorkSans-Bold': require('./assets/fonts/WorkSans-Bold.ttf'),
+    'WorkSans-SemiBold': require('./assets/fonts/WorkSans-SemiBold.ttf'),
+    'WorkSans-ExtraBold': require('./assets/fonts/WorkSans-ExtraBold.ttf'),
+    'WorkSans-Black': require('./assets/fonts/WorkSans-Black.ttf'),
+    // Agrega aquí más fuentes si las necesitas
+  });
+
+  await SplashScreen.hideAsync();
+}
---
+++ src/components/StyleText.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+import theme from '../theme'
+
+const styles = StyleSheet.create({
+    textoBioDefenders:{
+        color: theme.colors.colorLogo,
+        fontSize: theme.fontSizes.tituloNombreProyecto,
+        fontFamily: theme.fonts.workSansBold
+    },
+    titulo1:{
+        color: theme.colors.neutroEel,
+        fontSize: theme.fontSizes.titulo1,
+        fontFamily: theme.fonts.nunitoSansBlack,
+    },
+    titulo2:{
+        color: theme.colors.neutroEel,
+        fontSize: theme.fontSizes.titulo3,
+        fontFamily: theme.fonts.nunitoSansExtraBold,
+    },
+    titulo3:{
+        color: theme.colors.secondaryThree,
+        fontSize: theme.fontSizes.titulo2,
+        fontFamily: theme.fonts.workSansBold,
+    },
+    subTitulo1:{
+        color: theme.colors.neutroHare,
+        fontSize: theme.fontSizes.subTitulo2,
+        fontFamily: theme.fonts.nunitoSansBlack,
+    },
+    subTitulo2:{
+        color: theme.colors.neutroWolf,
+        fontSize: theme.fontSizes.subTitulo3,
+        fontFamily: theme.fonts.nunitoSansRegular,
+    },
+    textoGeneral:{
+        color: theme.colors.colorNegro,
+        fontSize: theme.fontSizes.textoGeneral,
+        fontFamily: theme.fonts.nunitoSansRegular,
+    },
+    textoInput:{
+        color: theme.colors.colorInput,
+        fontSize: theme.fontSizes.textoInput,
+        fontFamily: theme.fonts.nunitoSansBold,
+    },
+    textoBoton:{
+        color: theme.colors.colorNegro,
+        fontSize: theme.fontSizes.textoBoton,
+        fontFamily: theme.fonts.nunitoSansBold
+    }
+})
+
+export default function StyleText ({ children, style, tipo, ...restOfProps }) {
+    const textStyles = [
+        tipo === 'textoBioDefenders' && styles.textoBioDefenders,
+        tipo === 'titulo1' && styles.titulo1,
+        tipo === 'titulo2' && styles.titulo2,
+        tipo === 'titulo3' && styles.titulo3,
+        tipo === 'subTitulo1' && styles.subTitulo1,
+        tipo === 'subTitulo2' && styles.subTitulo2,
+        tipo === 'textoGeneral' && styles.textoGeneral,
+        tipo === 'textoInput' && styles.textoInput,
+        tipo === 'textoBoton' && styles.textoBoton,
+        style,  // Asegúrate de que 'style' sea el último para que pueda sobrescribir los estilos anteriores si es necesario
+    ]
+
+    return(
+        <Text style={textStyles} {...restOfProps}>
+            {children}
+        </Text>
+    )
+}
