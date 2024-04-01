e8cf950935d819fdac28200ad05db6261743a8df
bielma <abielmalopez@gmail.com>
2024-03-31 20:52:47 -0600

    Start:: add new view

---
+++ src/views/Start/Start.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Image, Text, TouchableHighlight, View } from "react-native";
+import { StartStyles } from "./constants";
+import StyleText from "../../components/StyleText";
+import { ButtonStyles } from "../../components/StyleButton";
+const Start = () => {
+  return (
+    <View style={StartStyles.container}>
+      <Image source={require("../../../assets/abranzando_mundo_1.png")} />
+      <StyleText tipo="titulo1" style={{ textAlign: "center" }}>
+        Empecemos la aventura
+      </StyleText>
+
+      <StyleText
+        tipo="subTitulo2"
+        style={{ textAlign: "center", marginTop: 15 }}
+      >
+        Diviértete mejorando el mundo
+      </StyleText>
+      <TouchableHighlight
+        style={[ButtonStyles.primaryButton, { marginTop: 35 }]}
+        activeOpacity={0.6}
+        underlayColor="#DDDDDD"
+        onPress={() => alert("Continuar!")}
+      >
+        <Text
+          style={{ color: "#F7F7F7", textAlign: "center", fontWeight: "bold" }}
+        >
+          CONTINUAR
+        </Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight
+        style={[ButtonStyles.secondaryButton, { marginTop: 10 }]}
+        activeOpacity={0.6}
+        underlayColor="#DDDDDD"
+        onPress={() => alert("Pressed!")}
+      >
+        <StyleText tipo="textSecondary" style={{ textAlign: "center" }}>
+          {" "}
+          Ya tengo una cuenta
+        </StyleText>
+      </TouchableHighlight>
+    </View>
+  );
+};
+
+export default Start;
---
+++ src/views/Start/constants/index.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const StartStyles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
