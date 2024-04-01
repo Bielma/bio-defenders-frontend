5ef005ed9d25af1c9594c47bcc90f52f65fe73d4
bielma <abielmalopez@gmail.com>
2024-03-31 20:52:04 -0600

    StyleText:: add text type

--- src/components/StyleText.jsx
+++ src/components/StyleText.jsx
@@ -3,54 +3,60 @@ import { Text, StyleSheet } from 'react-native'
 import theme from '../theme'
 
 const styles = StyleSheet.create({
-    textoBioDefenders:{
+    textoBioDefenders: {
         color: theme.colors.colorLogo,
         fontSize: theme.fontSizes.tituloNombreProyecto,
         fontFamily: theme.fonts.workSansBold
     },
-    titulo1:{
+    titulo1: {
         color: theme.colors.neutroEel,
         fontSize: theme.fontSizes.titulo1,
         fontFamily: theme.fonts.nunitoSansBlack,
     },
-    titulo2:{
+    titulo2: {
         color: theme.colors.neutroEel,
         fontSize: theme.fontSizes.titulo3,
         fontFamily: theme.fonts.nunitoSansExtraBold,
     },
-    titulo3:{
+    titulo3: {
         color: theme.colors.secondaryThree,
         fontSize: theme.fontSizes.titulo2,
         fontFamily: theme.fonts.workSansBold,
     },
-    subTitulo1:{
+    subTitulo1: {
         color: theme.colors.neutroHare,
         fontSize: theme.fontSizes.subTitulo1,
         fontFamily: theme.fonts.nunitoSansBlack,
     },
-    subTitulo2:{
+    subTitulo2: {
         color: theme.colors.neutroWolf,
         fontSize: theme.fontSizes.subTitulo3,
         fontFamily: theme.fonts.nunitoSansRegular,
     },
-    textoGeneral:{
+    textoGeneral: {
         color: theme.colors.colorNegro,
         fontSize: theme.fontSizes.textoGeneral,
         fontFamily: theme.fonts.nunitoSansRegular,
     },
-    textoInput:{
+    textoInput: {
         color: theme.colors.colorInput,
         fontSize: theme.fontSizes.textoInput,
         fontFamily: theme.fonts.nunitoSansBold,
     },
-    textoBoton:{
+    textoBoton: {
         color: theme.colors.colorBoton,
         fontSize: theme.fontSizes.textoBoton,
         fontFamily: theme.fonts.nunitoSansBold
+    },
+    textSecondary: {
+        color: theme.colors.secondaryOne,
+        fontSize: theme.fontSizes.textoBoton,
+        fontFamily: theme.fonts.nunitoSansBold
     }
+
 })
 
-export default function StyleText ({ children, style, tipo, ...restOfProps }) {
+export default function StyleText({ children, style, tipo, ...restOfProps }) {
     const textStyles = [
         tipo === 'textoBioDefenders' && styles.textoBioDefenders,
         tipo === 'titulo1' && styles.titulo1,
@@ -61,10 +67,11 @@ export default function StyleText ({ children, style, tipo, ...restOfProps }) {
         tipo === 'textoGeneral' && styles.textoGeneral,
         tipo === 'textoInput' && styles.textoInput,
         tipo === 'textoBoton' && styles.textoBoton,
+        tipo === 'textSecondary' && styles.textSecondary,
         style,  // Asegúrate de que 'style' sea el último para que pueda sobrescribir los estilos anteriores si es necesario
     ]
 
-    return(
+    return (
         <Text style={textStyles} {...restOfProps}>
             {children}
         </Text>
