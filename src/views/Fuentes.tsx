7d217e0e5d94f46e303d90bcd623012f1387e8a9
CaroJimenez <20213tn053@utez.edu.mx>
2024-03-31 19:35:36 -0600

    props fixed

--- src/views/Fuentes.tsx
+++ src/views/Fuentes.tsx
@@ -1,19 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import StyleText from '../components/StyleText.jsx'
+import { View, Text } from "react-native";
+import React from "react";
+import StyleText from "../components/StyleText.jsx";
 
 export default function SignUp() {
   return (
     <View>
-      <StyleText tipo='textoBioDefenders'>Texto Biodefenders</StyleText>
-      <StyleText tipo='titulo1'>Titulo 1 estilo</StyleText>
-      <StyleText tipo='titulo2'>Titulo 2 estilo</StyleText>
-      <StyleText tipo='titulo3'>Titulo 3 estilo</StyleText>
-      <StyleText tipo='subTitulo1'>Subtitulo 1 estilo</StyleText>
-      <StyleText tipo='subTitulo2'>Subtitulo 2 estilo</StyleText>
-      <StyleText tipo='textoInput'>Texto input</StyleText>
-      <StyleText tipo='textoBoton'>Texto botones</StyleText>
-      <StyleText tipo='textoGeneral'>Texto general estilo</StyleText>
+      <StyleText tipo="textoBioDefenders" style={{}}>
+        Texto Biodefenders
+      </StyleText>
+      <StyleText tipo="titulo1" style={{}}>
+        Titulo 1 estilo
+      </StyleText>
+      <StyleText tipo="titulo2" style={{}}>
+        Titulo 2 estilo
+      </StyleText>
+      <StyleText tipo="titulo3" style={{}}>
+        Titulo 3 estilo
+      </StyleText>
+      <StyleText tipo="subTitulo1" style={{}}>
+        Subtitulo 1 estilo
+      </StyleText>
+      <StyleText tipo="subTitulo2" style={{}}>
+        Subtitulo 2 estilo
+      </StyleText>
+      <StyleText tipo="textoInput" style={{}}>
+        Texto input
+      </StyleText>
+      <StyleText tipo="textoBoton" style={{}}>
+        Texto botones
+      </StyleText>
+      <StyleText tipo="textoGeneral" style={{}}>
+        Texto general estilo
+      </StyleText>
     </View>
-  )
+  );
 }
