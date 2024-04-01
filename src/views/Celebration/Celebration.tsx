046d32ab377a42e2bd079859e16afe060ac7aecd
bielma <abielmalopez@gmail.com>
2024-04-01 13:29:14 -0600

    Celebration:: add stats cards

--- src/views/Celebration/Celebration.tsx
+++ src/views/Celebration/Celebration.tsx
@@ -3,21 +3,78 @@ import { Image, Text, TouchableHighlight, View } from "react-native";
 import StyleText from "../../components/StyleText";
 import { ButtonStyles } from "../../components/StyleButton";
 import { CelebrationStyles } from "./constants";
+import theme from "../../theme";
 
-const Celebration = () => {
+interface ICelebrationProps {
+  name: string;
+  exp: number;
+  hits: number;
+}
+
+const Celebration = ({ name, exp, hits }: ICelebrationProps) => {
   return (
     <View style={CelebrationStyles.container}>
-      <View style={CelebrationStyles.fireworksContainer}>
-        <Image source={require("../../../assets/fuegos_artificiales.png")} />
-      </View>
-      <View style={CelebrationStyles.birdsContainer}>
-        <Image source={require("../../../assets/birds.png")} />
+      <View style={CelebrationStyles.imageContainer}>
+        <Image
+          style={CelebrationStyles.fireworksImage}
+          source={require("../../../assets/fuegos_artificiales.png")}
+        />
+        <Image
+          style={CelebrationStyles.birdsImage}
+          source={require("../../../assets/birds.png")}
+        />
       </View>
 
       <Image source={require("../../../assets/icon_mundo.png")} />
       <StyleText tipo="titulo3" style={CelebrationStyles.title}>
         Lección Completada!
       </StyleText>
+
+      <View
+        style={{
+          flexDirection: "row",
+          flex: 0.3,
+          width: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <View
+          style={[
+            CelebrationStyles.CardContainer,
+            { backgroundColor: theme.colors.naranjaDos, marginLeft: "3%" },
+          ]}
+        >
+          <StyleText
+            tipo={"textSecondary"}
+            style={{ textAlign: "center", color: "white" }}
+          >
+            TOTAL XP
+          </StyleText>
+          <View style={CelebrationStyles.CardBody}>
+            <StyleText tipo={"titulo3"} style={{}}>
+              {exp}
+            </StyleText>
+          </View>
+        </View>
+        <View
+          style={[
+            CelebrationStyles.CardContainer,
+            { backgroundColor: theme.colors.colorLogo, marginRight: "3%" },
+          ]}
+        >
+          <StyleText
+            tipo={"textSecondary"}
+            style={{ textAlign: "center", color: "white" }}
+          >
+            INCREIBLE
+          </StyleText>
+          <View style={CelebrationStyles.CardBody}>
+            <StyleText tipo={"titulo3"} style={{}}>
+              {hits}
+            </StyleText>
+          </View>
+        </View>
+      </View>
       <TouchableHighlight
         style={[ButtonStyles.primaryButton, CelebrationStyles.continueButton]}
         activeOpacity={0.6}
@@ -34,5 +91,6 @@ const Celebration = () => {
     </View>
   );
 };
+Celebration.defaultProps = { name: "", exp: 0, hits: 0 };
 
 export default Celebration;
--- src/views/Celebration/constants/index.ts
+++ src/views/Celebration/constants/index.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import theme from "../../../theme";
 
 export const CelebrationStyles = StyleSheet.create({
   container: {
@@ -7,6 +8,21 @@ export const CelebrationStyles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    width: "100%",
+  },
+  imageContainer: {
+    width: "100%",
+    display: "flex",
+    flex: 0.5,
+  },
+  birdsImage: {
+    left: "15%",
+    top: "50%",
+  },
+  fireworksImage: {
+    position: "absolute",
+    top: "10%",
+    right: "5%",
   },
   fireworksContainer: {
     display: "flex",
@@ -15,17 +31,35 @@ export const CelebrationStyles = StyleSheet.create({
     width: "100%",
   },
   birdsContainer: {
-    marginTop: -60,
-    marginRight: 60,
     display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    width: "100%",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    // flexDirection: "row",
+    // position: "absolute",
+    // left: "20%",
+    // top: "25%",
+    // width: "100%",
   },
   title: {
     marginTop: 35,
   },
   continueButton: {
-    marginTop: 35,
+    marginTop: "15%",
+  },
+  CardContainer: {
+    width: "45%",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    borderRadius: 40,
+    marginTop: "6%",
+  },
+  CardBody: {
+    marginTop: "4%",
+    width: "90%",
+    backgroundColor: "white",
+    borderRadius: 20,
+    alignItems: "center",
+    padding: "10%",
   },
 });
