f5c1334539e5166518629870ba8180c5c0ef8023
bielma <abielmalopez@gmail.com>
2024-04-03 06:09:54 -0600

    Boton:: add disabled prop

--- src/components/Boton/Boton.tsx
+++ src/components/Boton/Boton.tsx
@@ -1,49 +1,50 @@
-import {View, Text, TouchableHighlight, ViewStyle} from 'react-native'
-import React from 'react'
-import { TextStyle } from 'react-native'
-import { LoginStyles } from '../../views/Login/constants'
-import StyleText from '../StyleText'
+import { View, Text, TouchableHighlight, ViewStyle } from "react-native";
+import React from "react";
+import { TextStyle } from "react-native";
+import { LoginStyles } from "../../views/Login/constants";
+import StyleText from "../StyleText";
 
 interface IBotonProps {
-   
-    onPress: () => void;
-    style:ViewStyle;
-    tipoTexto: string;
-    textStyles: any;
-    activeOpacity: number;
-    underlayColor: string;
-    textValue: string;
+  onPress: () => void;
+  style: ViewStyle;
+  tipoTexto: string;
+  textStyles: any;
+  activeOpacity: number;
+  underlayColor: string;
+  textValue: string;
+  disabled: boolean;
 }
 
-
-
-const Boton = ({style, activeOpacity, underlayColor,textStyles,tipoTexto, onPress,textValue}:IBotonProps) => {
-return (
+const Boton = ({
+  style,
+  activeOpacity,
+  underlayColor,
+  textStyles,
+  tipoTexto,
+  onPress,
+  textValue,
+  disabled,
+}: IBotonProps) => {
+  return (
     <TouchableHighlight
-        style={style}
-        activeOpacity={activeOpacity}
-        underlayColor={underlayColor}
-        onPress={onPress}
+      disabled={disabled}
+      style={style}
+      activeOpacity={activeOpacity}
+      underlayColor={underlayColor}
+      onPress={onPress}
     >
-        <StyleText
-            tipo={tipoTexto}
-            style={textStyles}
-        >
-            {textValue}
-        </StyleText>
+      <StyleText tipo={tipoTexto} style={textStyles}>
+        {textValue}
+      </StyleText>
     </TouchableHighlight>
-)
-}
+  );
+};
 Boton.defaultProps = {
-    style: LoginStyles.primaryButton,
-    textStyles: {
+  style: LoginStyles.primaryButton,
+  textStyles: {},
+  activeOpacity: 0.6,
+  underlayColor: "#DDDDDD",
+  textValue: "CONTINUAR",
+};
 
-    },
-    activeOpacity: 0.6,
-    underlayColor: "#DDDDDD",
-    textValue: "CONTINUAR"
-}
-
-
-
-export default Boton
+export default Boton;
