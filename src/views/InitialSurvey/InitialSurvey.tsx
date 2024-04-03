7f928f4312a4173165c6b8014a66ebbc950663c9
bielma <abielmalopez@gmail.com>
2024-04-03 00:27:27 -0600

    InitialSurvey:: use questions.json

--- src/views/InitialSurvey/InitialSurvey.tsx
+++ src/views/InitialSurvey/InitialSurvey.tsx
@@ -1,27 +1,73 @@
 import React, { useEffect, useState } from "react";
-import { Animated, Text, View } from "react-native";
+import { Animated, Text, TextInput, TextInputBase, View } from "react-native";
 import { InitialSurveyStyles } from "./constants";
 import ProgressBar from "../../components/ProgressBar";
 import { Dimensions } from "react-native";
 import Boton from "../../components/Boton";
+import questions from "../../constants/questions";
+import StyleText from "../../components/StyleText";
+import { LoginStyles } from "../../views/Login/constants";
 interface Question {
   question: string;
   options: string[];
 }
 const InitialSurvey = () => {
-  const [questions, setQuestions] = useState<Question[]>([]);
-  const [progress, setProgress] = useState<number>(0.8);
-
+  const [questionsHome, setQuestionsHome] = useState<Question[]>([]);
+  const [progress, setProgress] = useState<number>(0);
+  const [questionNumber, setQuestionNumber] = useState<number>(0);
+  const [responses, setResponses] = useState<string[]>([]);
+  useEffect(() => {
+    setQuestionsHome(questions.casa);
+  }, []);
   const handlePress = () => {
-    setProgress((prevProgress) => prevProgress + 0.1);
+    if (questionNumber === questionsHome.length - 1) {
+      alert("Continuar!");
+      return;
+    }
+    setProgress(
+      (prevProgress) => prevProgress + 0.8 / (questionsHome.length - 1)
+    );
+
+    setQuestionNumber((prevQuestionNumber) => prevQuestionNumber + 1);
   };
 
   return (
     <View style={InitialSurveyStyles.container}>
-      <ProgressBar progress={progress} />
-      <Text>Initial Survey</Text>
+      <View style={InitialSurveyStyles.BarContainer}>
+        <ProgressBar progress={progress} />
+      </View>
+      <View style={InitialSurveyStyles.section}>
+        <StyleText tipo={"titulo1"} style={{}}>
+          {questionsHome[questionNumber]?.question}
+        </StyleText>
+      </View>
+      <View style={InitialSurveyStyles.section}>
+        {questionsHome[questionNumber]?.options.length > 0 ? (
+          questionsHome[questionNumber]?.options.map((option, index) => {
+            return (
+              <Boton
+                style={LoginStyles.googleButton}
+                key={index}
+                tipoTexto="titulo3"
+                textStyles={{
+                  textAlign: "center",
+                  wcolor: "white",
+                }}
+                onPress={() => {
+                  setResponses((prevResponses) => [...prevResponses, option]);
+                  handlePress();
+                }}
+                textValue={option}
+              />
+            );
+          })
+        ) : (
+          <TextInput style={InitialSurveyStyles.input} />
+        )}
+      </View>
+
       <Boton
-        tipoTexto="titulo1"
+        tipoTexto="titulo2"
         textStyles={{ textAlign: "center", color: "white" }}
         onPress={handlePress}
         textValue="Continuar"
--- src/views/InitialSurvey/constants/index.ts
+++ src/views/InitialSurvey/constants/index.ts
@@ -7,6 +7,7 @@ export const InitialSurveyStyles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     width: "100%",
+    justifyContent: "space-between",
   },
   title: {
     fontSize: 20,
@@ -14,14 +15,28 @@ export const InitialSurveyStyles = StyleSheet.create({
   },
 
   BarContainer: {
-    backgroundColor: "#ccc",
     borderRadius: 10,
-    margin: 10,
-    width: "80%",
+    marginTop: "7%",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
   },
   bar: {
     height: 20,
     backgroundColor: "#333",
     borderRadius: 10,
+  },
+  section: {
+    width: "100%",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    height: 58,
+    marginTop: "5%",
+    borderWidth: 2,
+    borderRadius: 16,
+    borderColor: "#E5E5E5",
+    backgroundColor: "#F7F7F7",
   },
 });
