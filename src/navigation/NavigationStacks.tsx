e0826b50622b8948c410c842cb6a1eb8a18d62f9
CaroJimenez <20213tn053@utez.edu.mx>
2024-04-03 14:03:08 -0600

    stacks added

--- src/navigation/NavigationStacks.tsx
+++ src/navigation/NavigationStacks.tsx
@@ -3,6 +3,8 @@ import { SafeAreaView, TouchableOpacity } from "react-native";
 import { NAVIGATION_SCREEN } from "./constants";
 import Login from "../views/Login";
 import Home from "../views/Home";
+import SignUp from "../views/Fuentes";
+import Start from "../views/Start/Start";
 
 const Stack = createNativeStackNavigator();
 const NavigationStacks = () => {
@@ -10,6 +12,8 @@ const NavigationStacks = () => {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" component={Login} />
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Signup" component={SignUp} />
+      <Stack.Screen name="Start" component={Start} />
     </Stack.Navigator>
   );
 };
