7273801a6cabb1b09c74f603ac6a9ad31702e1d6
bielma <abielmalopez@gmail.com>
2024-04-04 00:00:01 -0600

    Update frame image paths in Home constants

--- src/views/Home/constants/index.ts
+++ src/views/Home/constants/index.ts
@@ -2,16 +2,16 @@ import { StyleSheet } from "react-native";
 import theme from "../../../theme";
 
 export const frames = {
-  frame1: require("../../../../assets/frames/framePlanta_01.jpg"),
-  frame2: require("../../../../assets/frames/framePlanta_02.jpg"),
-  frame3: require("../../../../assets/frames/framePlanta_03.jpg"),
-  frame4: require("../../../../assets/frames/framePlanta_04.jpg"),
-  frame5: require("../../../../assets/frames/framePlanta_05.jpg"),
-  frame6: require("../../../../assets/frames/framePlanta_06.jpg"),
-  frame7: require("../../../../assets/frames/framePlanta_07.jpg"),
-  frame8: require("../../../../assets/frames/framePlanta_08.jpg"),
+  frame1: require("../../../../assets/frames/version_gratuita/plantas/version_01/frame01_v1.png"),
+  frame2: require("../../../../assets/frames/version_gratuita/plantas/version_01/frame02_v1.png"),
+  frame3: require("../../../../assets/frames/version_gratuita/plantas/version_01/frame03_v1.png"),
+  frame4: require("../../../../assets/frames/version_gratuita/plantas/version_01/frame04_v1.png"),
+  frame5: require("../../../../assets/frames/version_gratuita/plantas/version_01/frame05_v1.png"),
+  frame6: require("../../../../assets/frames/version_gratuita/plantas/version_01/frame06_v1.png"),
+  frame7: require("../../../../assets/frames/version_gratuita/plantas/version_01/frame07_v1.png"),
+  frame8: require("../../../../assets/frames/version_gratuita/plantas/version_01/frame08_v1.png"),
 };
-
+//frames/version_gratuita/plantas/version_01/frame01_v1.png
 export const HomeStyles = StyleSheet.create({
   container: {
     flex: 1,
